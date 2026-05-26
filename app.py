18f824d17cb49622547eda207aae1902778c56d5
samia zahid <samiaazahid01@gmail.com>
2026-05-26 17:54:12 +0500

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -1097,7 +1097,7 @@ with tab3:
     
     # 1. MODEL SETUP (Yeh line zaroor honi chahiye, is se model define hota hai)
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
+        model_name="gemini-2.5-flash",
         system_instruction=system_instruction
     )
     
